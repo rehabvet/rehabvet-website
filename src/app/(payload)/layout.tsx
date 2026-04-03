df8d1dc80dd137925d7610f5ef269992515d6db0
Marcus Lim <marcus@Marcuss-Mac-mini-2.local>
2026-04-03 17:27:46 +0800

    fix: add importMap and serverFunction props to Payload admin layout

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,9 +1,10 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import type { ServerComponentProps } from 'payload'
+import type { ServerFunctionClient } from 'payload'
 
 import config from '@payload-config'
-import { RootLayout } from '@payloadcms/next/layouts'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import { importMap } from './admin/importMap'
 import React from 'react'
 
 import './custom.scss'
@@ -12,6 +13,19 @@ type Args = {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Args) => <RootLayout config={config}>{children}</RootLayout>
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
+const Layout = ({ children }: Args) => (
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    {children}
+  </RootLayout>
+)
 
 export default Layout
